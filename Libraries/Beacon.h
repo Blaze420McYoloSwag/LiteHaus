b3e737aece80402bf7b79dc5415ee58509ffeeb1
Blaze420McYoloSwag <tnealon80@gmail.com>
2017-02-23 10:24:35 -0500

    Create Beacon.h

---
+++ Libraries/Beacon.h
@@ -0,0 +1,18 @@
+#ifndef BEACON_H
+#define BEACON_H
+
+#include Adafruit_Neopixel.h
+
+class pixelRing: public Adafruit_NeoPixel {
+    public:
+        pixelRing(uint16_t numPixels, uint8_t pin, uint16_t type)
+        : Adafruit_NeoPixel(numPixels,pin,type){};
+        void ringShuffle();
+        uint32_t dimStep(uint32_t color, int stepNum, int steps); //This reduces the inputted color
+        void dimRing(int stepNum, int steps);
+        uint8_t getRed(uint32_t color);
+        uint8_t getBlue(uint32_t color);
+        uint8_t getGreen(uint32_t color);
+};
+
+#endif
