1ea214d2c7a2398fcb2e6e678e9ba625afa70e20
Blaze420McYoloSwag <tnealon80@gmail.com>
2017-03-14 16:03:28 -0400

    Update Receive_test.ino

--- FastLED/Receive_test.ino
+++ FastLED/Receive_test.ino
@@ -5,9 +5,13 @@ FASTLED_USING_NAMESPACE;
 int receivedHue = 0;
 int receivedSaturation = 0;
 int receivedValue = 0;
+int baseLightsLit = 0; //This value is the number of light lit in the base
 
-String receivedID;
 CHSV baseColor;
+
+
+String selfName = "Bonnie"; //The name of this device
+String otherName = "Tim"; //The name of the device you're listening to
 
 void setup() {
     Particle.subscribe("Beacon Update", ColorUpdate, MY_DEVICES);
@@ -21,7 +25,9 @@ void loop() {
 
 
 void ColorUpdate(const char *name, const char *data) {
+    String receivedID;
     if (data) {
+        
         char input[64];
         strcpy(input,data);
         char *p;
@@ -36,7 +42,10 @@ void ColorUpdate(const char *name, const char *data) {
 
     }
     // DEBUG
-    Particle.publish("Color_Recieved", System.deviceID() + "~" + receivedHue + "~" + receivedSaturation + "~" + receivedValue);
+    Particle.publish("Color_Recieved", selfName + "~" + receivedHue + "~" + receivedSaturation + "~" + receivedValue);
     // END DEBUG
-    baseColor = CHSV(receivedHue, receivedSaturation, receivedValue);
+    if(receivedID ==otherName ){
+        baseColor = CHSV(receivedHue, receivedSaturation, receivedValue);
+    }
 }
+
